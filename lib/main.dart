afca3f2318e6732d716e664a4f3df5fdb95a0bbf
Arun Kumar <arun.kumar@gmail.com>
2024-04-03 18:21:41 +0530

    MVVM - Repository pattern, with Provider package, ChangeNotifier Provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:test_temp/feature/view_modal/home_view_modal.dart';
 
 import 'feature/presentation/home_screen.dart';
 void main() {
