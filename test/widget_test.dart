037be44ab3b00adf0c818f45b83bfdad4471703d
Arun Kumar <arun.kumar@gmail.com>
2024-04-05 15:33:22 +0530

    Data Structure: find duplicate and pair sum

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -12,31 +12,38 @@ import 'package:test_temp/main.dart';
 
 void main() async {
   print("Hello world");
-  List<String> arrBands = ["Metallica", "Foo Fighters", "Pink Floyd", "Nirvana"];
+  List<int> arr = [5,4,3,2,3,6,1];
+ // print("Duplicate Item = ${findDuplicateNum(arr)}");
+  print("Pair Sum = ${getPairSum(arr, 7)}");
 
-   await for (String name in getNames()) {
-    print("$name \n");
+}
+
+List<int> findDuplicateNum(List<int> arr){
+  List<int> arrTemp =[];
+  for(int i = 0; i<= arr.length -1 ; i++){
+    int toVisit = arr[i].abs() - 1;
+
+    if(arr[toVisit] < 0 ){
+      arrTemp.add(arr[toVisit].abs());
+    }else {
+      arr[toVisit] = arr[toVisit] * -1;
+    }
   }
-
-  List<int> arr = List.filled(5, 1);
-   print(arr);
-
-  List<int> l = [1, 2, 3, 4];
-  List<int> r = [3, 4];
-
-  l.removeWhere((element) => r.contains(element));
- print("Value is $l");
-
+  return arrTemp;
 }
+List<int> getPairSum(List<int> arr, int num) {
+  //[5,4,3,2,3,6,1] -> 7
+  List<int> arrToReturn = [];
+  Map<int,int> dict = {};
+  for(int i = 0 ; i <= arr.length -1 ; i++) {
+    int target = num - arr[i];
+    if(dict[target] == null) {
+      dict[arr[i]] = 0;
+    }else {
+      arrToReturn.add(target);
+      arrToReturn.add(arr[i]);
+    }
 
-Stream<String> getNames() async* {
-
-  await Future.delayed(const Duration(seconds: 1));
-  yield "Arun";
-  await Future.delayed(const Duration(seconds: 3));
-  yield "Varun";
-
+  }
+  return arrToReturn;
 }
-
-
-
