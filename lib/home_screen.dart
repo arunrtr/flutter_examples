8b176a3c87ff4b050bdab087f12baa492439f351
Arun Kumar <arun.kumar@gmail.com>
2024-04-08 10:19:46 +0530

    Example: Object Key

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -15,12 +15,12 @@ class _HomeScreenState extends State<HomeScreen> {
     super.initState();
     widget.arrChildren = [
       Padding(
-        key: UniqueKey(),
+        key: ObjectKey(1),
         padding: const EdgeInsets.all(8.0),
         child: ColorWidget( num: 1,),
       ),
       Padding(
-        key: UniqueKey(),
+        key: ObjectKey(2),
         padding: const EdgeInsets.all(8.0),
         child: ColorWidget( num: 2,),
       )
