78543aa32391996145caf214da30166d6c14106d
arunrtr <arunrtr@gmail.com>
2025-01-19 00:13:13 +0530

    good pairs, n*(n-1)/2

--- test/02_array.dart
+++ test/02_array.dart
@@ -1,19 +1,36 @@
-
 void main() {
-
-  List<int> arr = reverseArray([1,2,3,4]);
-  print("The reverse of $arr");
-
+ // print("The reverse of ${reverseArray([1, 2, 3, 4])}");
+  print("The good pair is ${goodPair([1, 2, 3, 1, 1, 3])}");
 }
 
+// good pairs
+//https://www.youtube.com/watch?v=S-NGf0yWXN8
+int goodPair(List<int> arr) {
+  int numOfPair = 0;
+  Map<int, int> dict = {};
+  for (int i = 0; i < arr.length; i++) {
+    if (dict.containsKey(arr[i])) {
+      dict[arr[i]] = dict[arr[i]]! + 1;
+    } else {
+      dict[arr[i]] = 1;
+    }
+  }
 
-List<int> reverseArray(List<int> arr){
-  for(int i = 0 ; i< ((arr.length -1) * 0.5) ; i++) {
-    print("inside");
+  print("My Good Pair dict = $dict");
+
+  dict.forEach((key, value) {
+    numOfPair +=  (value * (value -1))~/2; // this is the main thing here, n*(n-1)/2
+  });
+
+  return numOfPair;
+}
+
+//   reverse array
+List<int> reverseArray(List<int> arr) {
+  for (int i = 0; i < ((arr.length - 1) * 0.5); i++) {
     arr[i] = arr[i] + arr[arr.length - 1 - i];
-    arr[arr.length -1 - i] = arr[i] - arr[arr.length - 1 - i];
-    arr[i] = arr[i] -arr[arr.length - 1 - i];
-    print(arr[i]);
+    arr[arr.length - 1 - i] = arr[i] - arr[arr.length - 1 - i];
+    arr[i] = arr[i] - arr[arr.length - 1 - i];
   }
   return arr;
 }
